2152b99406efb3fd55c343bc5a4ca7c079384bb4
Belmiro Mungoi <belmiromungoi@gmail.com>
2021-10-15 21:33:05 +0200

    adicionando o componente Usuario

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,15 @@ import { HttpClientModule } from "@angular/common/http";
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component'; //Para as requisicoes Ajax
 import { HttpInterceptorModule } from './service/header-interceptor.service';
+import { UsuarioComponent } from './component/usuario/usuario.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    LoginComponent
+    LoginComponent,
+    UsuarioComponent
   ],
   imports: [
     BrowserModule,
