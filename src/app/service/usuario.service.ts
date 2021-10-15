02e2d68d689e79d138c7e814860035350b4524d4
Belmiro Mungoi <belmiromungoi@gmail.com>
2021-10-15 21:35:35 +0200

    servico para para trazer a lista de usuarios com a url

---
+++ src/app/service/usuario.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { AppConstants } from '../app-constants';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsuarioService {
+
+  constructor(private http: HttpClient) { }
+
+  getUserList(): Observable<any> {
+    return this.http.get<any>(AppConstants.baseUrl);
+  }
+}
