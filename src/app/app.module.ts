1457ecad62d35076003343f91b4d5256a83b4a47
Belmiro Mungoi <belmiromungoi@gmail.com>
2021-10-15 12:53:33 +0200

    adicionanco a nova classe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from "@angular/common/http";
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component'; //Para as requisicoes Ajax
+import { HttpInterceptorModule } from './service/header-interceptor.service';
 
 
 @NgModule({
@@ -19,7 +20,8 @@ import { LoginComponent } from './login/login.component'; //Para as requisicoes 
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    HttpInterceptorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
