c26bf4e0e7fad78cdcf591f9a66e966dd0387cd9
Belmiro Mungoi <belmiromungoi@gmail.com>
2021-10-15 09:48:37 +0200

    removendo o codigo para outro componente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { LoginService } from './service/login.service';
 
 @Component({
   selector: 'app-root',
@@ -9,11 +8,5 @@ import { LoginService } from './service/login.service';
 export class AppComponent {
   title = 'Crud-Rest-Angular';
 
-  usuario = { userName: '', passWord: '' };
-
-  constructor(private loginService: LoginService) { }
-
-  public login() {
-    this.loginService.login(this.usuario)
-  }
+  
 }
