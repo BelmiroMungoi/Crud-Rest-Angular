11240262f844cfa1f29ecfff02b2dece299f9c41
Belmiro Mungoi <belmiromungoi@gmail.com>
2021-10-15 09:48:01 +0200

    adicionando as rotas do sistema

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: '', component: LoginComponent }
+
+];
+
+//export const appRoutes : ModuleWithProviders = RouterModule.forRoot(routes);
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
