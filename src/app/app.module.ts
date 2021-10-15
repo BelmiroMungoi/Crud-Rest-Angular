86cb0e88a619eb11ff7a9d30f1382a112f0ce15d
Belmiro Mungoi <belmiromungoi@gmail.com>
2021-10-15 09:48:52 +0200

    adicionando novos componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from "@angular/common/http";
-import { HomeComponent } from './home/home.component'; //Para as requisicoes Ajax
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component'; //Para as requisicoes Ajax
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
