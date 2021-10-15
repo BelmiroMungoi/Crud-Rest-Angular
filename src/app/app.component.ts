c21aef0a6ab0baaba03e884c661f9fcb9a407523
Belmiro Mungoi <belmiromungoi@gmail.com>
2021-10-15 11:50:07 +0200

    rederecionando a pagina de login caso nao exista um token

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'Crud-Rest-Angular';
 
-  
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+
+    if (localStorage.getItem('token') == null) {
+      this.router.navigate(['login']);
+    }
+  }
+
+  public sair(){
+    localStorage.clear();
+    this.router.navigate(['login']);
+  }
+
 }
