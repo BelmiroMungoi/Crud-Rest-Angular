37d250672a3f175bf264badd072a2c0c40a43eea
Belmiro Mungoi <belmiromungoi@gmail.com>
2021-11-03 21:48:58 +0200

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from "@angular/common/http";
 import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './login/login.component'; //Para as requisicoes Ajax
+import { LoginComponent } from './login/login.component';
 import { HttpInterceptorModule } from './service/header-interceptor.service';
 import { UsuarioComponent } from './component/usuario/usuario.component';
 import { UsuarioAddComponent } from './component/usuario-add/usuario-add.component';
