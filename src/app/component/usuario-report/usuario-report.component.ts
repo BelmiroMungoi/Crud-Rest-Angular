dd70955742587c568d6e8fa836008f5c230e7aca
Belmiro Mungoi <belmiromungoi@gmail.com>
2022-01-18 20:53:07 +0200

    imprimindo o relatorio

--- src/app/component/usuario-report/usuario-report.component.ts
+++ src/app/component/usuario-report/usuario-report.component.ts
@@ -1,11 +1,8 @@
-import { Component, Injectable, OnInit } from '@angular/core';
+import { Component, Injectable } from '@angular/core';
 import { ActivatedRoute } from "@angular/router";
-import { Usuario } from 'src/app/model/usuario';
 import { UsuarioService } from 'src/app/service/usuario.service';
-import { Endereco } from 'src/app/model/endereco'
 import { NgbDateAdapter, NgbDateParserFormatter, NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
-import { Profissao } from 'src/app/model/profissao';
-import { FormBuilder } from '@angular/forms';
+import { UserReport } from 'src/app/model/userReport';
 
 @Injectable()
 export class FormatDateAdapter extends NgbDateAdapter<string>{
@@ -71,77 +68,14 @@ function validaDate(valor: any) {
   providers: [{ provide: NgbDateParserFormatter, useClass: FormataData },
   { provide: NgbDateAdapter, useClass: FormatDateAdapter }]
 })
-export class UsuarioReportComponent implements OnInit {
+export class UsuarioReportComponent {
 
-  usuario = new Usuario();
+  userReport = new UserReport();
 
-  endereco = new Endereco();
+  constructor(private routeActive: ActivatedRoute, private userService: UsuarioService) { }
 
-  profissoes!: Array<Profissao>;
-
-  constructor(private routeActive: ActivatedRoute, private userService: UsuarioService, public fb: FormBuilder) { }
-
-  regForm = this.fb.group({
-    profName: ['']
-  })
-  ngOnInit(): void {
-
-    this.userService.getProfissaoList().subscribe(data => {
-      this.profissoes = data;
-    })
-
-    let id = this.routeActive.snapshot.paramMap.get('id');
-
-    if (id !== null) {
-      this.userService.getUserById(id).subscribe(data => {
-        this.usuario = data;
-      });
-    }
-  }
-
-  saveUser() {
-    if (this.usuario.id != null && this.usuario.id.toString().trim() != null) {
-      this.userService.updateUser(this.usuario).subscribe(data => {
-        this.cancelar();
-        console.log("Editando usuario " + data)
-      });
-    } else {
-
-      this.userService.saveUser(this.usuario).subscribe(data => {
-        this.cancelar();
-        console.log("Salvando Usuario " + data)
-      });
-    }
-  }
-
-  addEndereco() {
-
-    if (this.usuario.enderecos === undefined) {
-      this.usuario.enderecos = new Array<Endereco>();
-    }
-
-    this.usuario.enderecos.push(this.endereco);
-    this.endereco = new Endereco();
-  }
-
-  deletarEndereco(id: any, i: any) {
-
-    if (id == null) {
-      this.usuario.enderecos.splice(i, 1);
-      return;
-    }
-
-    if (id != null && confirm("Deseja Remover?")) {
-      this.userService.deleteEndereco(id).subscribe(data => {
-        this.usuario.enderecos.splice(i, 1);
-        console.info("Endereco Removido " + data);
-      })
-    }
-  }
-
-  cancelar() {
-    this.usuario = new Usuario();
-    this.endereco = new Endereco();
+  printReportParam(){
+    this.userService.downloadPdfReportParam(this.userReport);
   }
 
 }
