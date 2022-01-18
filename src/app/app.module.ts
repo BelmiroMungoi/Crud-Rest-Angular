19120ee4349cb97d1021b95f744caee4ea093d50
Belmiro Mungoi <belmiromungoi@gmail.com>
2022-01-18 15:25:32 +0200

    adicionando uma nova classe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from "@angular/common/http";
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
@@ -29,6 +29,7 @@ export const optionMask: Partial<IConfig> | (() => Partial<IConfig>) = {};
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpClientModule,
     HttpInterceptorModule,
     NgxMaskModule.forRoot(optionMask),
