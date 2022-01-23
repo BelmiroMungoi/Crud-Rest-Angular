420c459c0bfbde38d88208e22c85571ef94cdaf4
Belmiro Mungoi <belmiromungoi@gmail.com>
2022-01-23 11:02:01 +0200

    configurando o grafico

--- src/app/component/bar-chart/bar-chart.component.ts
+++ src/app/component/bar-chart/bar-chart.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ChartOptions, ChartType, ChartDataSets } from 'chart.js';
+import { Label } from 'ng2-charts';
 
 @Component({
   selector: 'app-bar-chart',
@@ -11,5 +13,16 @@ export class BarChartComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  barChartOptions: ChartOptions = {
+    responsive: true,
+  };
+  barChartLabels: Label[] = ['Apple', 'Banana', 'Kiwifruit', 'Blueberry', 'Orange', 'Grapes'];
+  barChartType: ChartType = 'bar';
+  barChartLegend = true;
+ //barChartPlugins = [];
+
+  barChartData: ChartDataSets[] = [
+    { data: [45, 37, 60, 70, 46, 33], label: 'Salário Usuário' }
+  ];
 
 }
