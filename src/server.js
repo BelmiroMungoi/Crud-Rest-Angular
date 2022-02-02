39aa5d86e54e0d06b1f43b287d2fb8360ada1a29
Belmiro Mungoi <belmiromungoi@gmail.com>
2022-02-02 11:14:34 +0200

    atualizando para deploy

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const path = require('path')
+const path = require('path');
 
 const app = express();
 
@@ -9,4 +9,4 @@ app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname + '/dist/Crud-Rest-Angular/index.html'));
 });
 
-app.listen(process.env.PORT || 8080)
+app.listen(process.env.PORT || 8080);
