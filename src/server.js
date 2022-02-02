59211b8dd1da8a95e9f5d86a97df87554a92fdea
Belmiro Mungoi <belmiromungoi@gmail.com>
2022-02-02 11:48:58 +0200

    Update server.js

--- src/server.js
+++ src/server.js
@@ -3,10 +3,10 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist/Crud-Rest-Angular'));
+app.use(express.static(__dirname + '/dist/crud-rest-angular'));
 
 app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname + '/dist/Crud-Rest-Angular/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/crud-rest-angular/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
